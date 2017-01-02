5c2ef0977a1426e3ea0929d053b23db413ffc757
Peter Phalen <PeterPhalen@Peters-MacBook-Air-2.local>
2017-01-01 21:43:05 -0500

    add better commenting

--- main.py
+++ main.py
@@ -10,10 +10,6 @@ import urllib2
 from google.appengine.api import app_identity
 #[END imports]
 
-killings = urllib2.urlopen("https://raw.githubusercontent.com/joshbegley/the-counted/master/skeleton.json")
-data = json.load(killings)  
-
-
 
 webpage_header = r"""<!DOCTYPE html>
                 <html>          
@@ -35,34 +31,36 @@ webpage_header = r"""<!DOCTYPE html>
                 <meta property="twitter:description" content="Interactive map of police killings in the United States. Generated using data from The Guardian'\s The Counted project."/> 
                 <meta name="twitter:image" content="https://www.peterphalen.com/images/police-killings-map-img.png"/> type" content="text/html; charset=UTF-8" />
                  """
-        
-            
+
+
+police_killings = urllib2.urlopen("https://raw.githubusercontent.com/joshbegley/the-counted/master/skeleton.json")
+data = json.load(police_killings)  
+
+
+#[START map definition]
+#draw map centered around the contiguous USA
+map_killings = folium.Map(location=[39.8282, -98.5795], zoom_start=5) 
+
+#iterate through markers, simple pop-ups with names of people who died
+for i in range(0, len(data)):
+    map_killings.simple_marker([data[i]['lat'], data[i]['long']], popup=data[i]['name'])
+#[END map definition]
 
 
 #[START retries]
-my_default_retry_params = gcs.RetryParams(initial_delay=5,
-                                          max_delay=30,
+my_default_retry_params = gcs.RetryParams(initial_delay=1,
+                                          max_delay=15,
                                           backoff_factor=2,
                                           max_retry_period=200)
 gcs.set_default_retry_params(my_default_retry_params)
 #[END retries]
 
 
-#[MAP definition]
-map_killings = folium.Map(location=[39.8282, -98.5795], zoom_start=5)
-
-#iterate markers
-for i in range(0, len(data)):
-    map_killings.simple_marker([data[i]['lat'], data[i]['long']], popup=data[i]['name'])
-
-
 class MainPage(webapp2.RequestHandler):
-    """Main page for GCS demo application."""
 
-    #[START get_default_bucket]
     def get(self):
 
-
+        #get GCS bucket to save map html 
         bucket_name = os.environ.get('BUCKET_NAME',
                                      app_identity.get_default_gcs_bucket_name())
 
@@ -72,8 +70,9 @@ class MainPage(webapp2.RequestHandler):
         filename = bucket + '/demo-testfile'
         self.tmp_filenames_to_clean_up = []
 
+        #try creating/saving the map via folium and then reading it back into str
         try:
-            self.create_file(filename)
+            self.save_map(filename)
             self.response.write('\n\n')
 
             gcs_file = gcs.open(filename)
@@ -81,23 +80,20 @@ class MainPage(webapp2.RequestHandler):
             map_html = map_html[150:len(map_html)] #drop opening headers
             full_webpage_html = webpage_header + map_html + "\n\n</html>"
 
-            self.response.write(full_webpage_html)
+            self.response.write(full_webpage_html) # <-- write out a complete static HTML page 
+                                                   #     displaying interactive map and meta tags 
             gcs_file.close()
-            self.response.write('\n\n')
 
 
         except Exception, e:
           logging.exception(e)
-          self.response.write('\n\nThere was an error running the demo! '
-                              'Please check the logs for more details.\n')
+          self.response.write('\n\nThis web app produced an error. Developers: check log for more information.\n')
 
         else:
           pass
 
-    #[START write]
-    def create_file(self, filename):
-
-
+    #[START write file function]
+    def save_map(self, filename):
         write_retry_params = gcs.RetryParams(backoff_factor=1.1)
         gcs_file = gcs.open(filename,
                         'w',
@@ -105,7 +101,7 @@ class MainPage(webapp2.RequestHandler):
                         options={'x-goog-meta-foo': 'foo',
                                  'x-goog-meta-bar': 'bar'},
                         retry_params=write_retry_params)
-        map_killings.create_map(gcs_file)
+        map_killings.create_map(gcs_file) # <-- save the result of the above folium call 
         gcs_file.close()
         self.tmp_filenames_to_clean_up.append(filename)
     #[END write]
